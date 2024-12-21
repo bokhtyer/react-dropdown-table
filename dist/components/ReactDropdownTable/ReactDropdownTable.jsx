7e3aab7d559d6b090239da91bd40b98464e48a78
Bokhtyer Abid <68472916+bokhtyer@users.noreply.github.com>
2024-12-21 09:58:40 +0600

    issue fix

--- dist/components/ReactDropdownTable/ReactDropdownTable.jsx
+++ dist/components/ReactDropdownTable/ReactDropdownTable.jsx
@@ -2,7 +2,6 @@
 import { ReactElement, ReactNode, useEffect, useRef, useState } from "react";
 import "./ReactDropdownTable.css";
 import { handleClickOutside } from "../../helpers/helper";
-// import DropdownArrow from "../../helpers/DropdownArrow";
 import React from "react";
 
 export default function ReactDropdownTable(props) {
@@ -104,7 +103,11 @@ export default function ReactDropdownTable(props) {
                 onClick={() => setActive(!active)}
             >
                 {button}
-                {/* {arrow && <DropdownArrow />} */}
+                {arrow && (
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                        <path d="M201.4 374.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 306.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" />
+                    </svg>
+                )}
             </div>
 
             <div
